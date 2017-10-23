ec5ba58e1f3a80840a13ece67c3d53275b747740
mostafakhattab00 <mostafa.khattab@ibtikar.net.sa>
2017-10-23 03:43:01 +0200

    add load balance

---
+++ load-balance/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const pid = process.pid;
+
+http.createServer((req, res) => {
+	for(let i = 0 ; i < 1e7; ++i);
+	res.end(`Handled by process ${pid}`);
+}).listen(8080, () => {
+	console.log(`Server process ${pid}`);
+});
+
+// setTimeout(() => {
+// 	process.exit(1);
+// }, Math.random() * 10000);
